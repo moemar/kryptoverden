85d96f1d89379011f801ff4cafaf831dfb283b2a
Martin Moen <magmo@eg.no>
2019-08-02 20:11:41 +0200

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Kryptoverden`,
+    author: `Kryptoverden`,
+    description: `Kryptoverden.no`,
+    siteUrl: `https://kryptoverden.no/`,
   },
   plugins: [
     {
@@ -50,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-145086742-1`,
       },
     },
     `gatsby-plugin-feed`,
